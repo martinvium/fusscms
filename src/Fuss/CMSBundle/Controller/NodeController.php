680f5dd8d13ff665dfd7e2fc8043312fe43027e4
Martin Vium <martin.vium@gmail.com>
2014-05-18 00:14:56 +0200

    Expose node title to view and use it

--- src/Fuss/CMSBundle/Controller/NodeController.php
+++ src/Fuss/CMSBundle/Controller/NodeController.php
@@ -11,9 +11,10 @@ class NodeController extends Controller
         $filename = $this->get('kernel')->getRootDir() . '/nodes/articles/' . $slug . '.md';
         $text = file_get_contents($filename);
         $node = $this->container->get('node.parser')->parseNode($text);
-        
+
         return $this->render('FussCMSBundle:Node:show.html.twig', array(
-          'node_content' =>  $node->getBody()
+          'node_content' =>  $node->getBody(),
+          'node_title' => $node->getField('title')
         ));
     }
 }
--- src/Fuss/CMSBundle/Models/Node.php
+++ src/Fuss/CMSBundle/Models/Node.php
@@ -7,8 +7,23 @@ class Node
     private $fields = array();
     private $body;
 
-    public function setFields(array $fields) { $this->fields = $fields; }
-    public function getFields() { return $this->fields; }
-    public function setBody($body) { $this->body = $body; }
-    public function getBody() { return $this->body; }
+    public function setFields(array $fields) {
+        $this->fields = $fields;
+    }
+    
+    public function getFields() { 
+        return $this->fields;
+    }
+    
+    public function setBody($body) {
+        $this->body = $body;
+    }
+    
+    public function getBody() { 
+        return $this->body;
+    }
+
+    public function getField($name) {
+        return $this->fields[$name];
+    }
 }
